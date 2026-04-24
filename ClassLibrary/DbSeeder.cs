f2a5debca82e16c2442f472ee798bab707dbfafe
gert Rasmussen <pjente@gmail.com>
2026-04-24 18:46:34 +0200

    Add project files.

---
+++ ClassLibrary/DbSeeder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+
+
+    public static class DbSeeder
+
+    {
+
+
+        public static void Seed(AppDbContext context)
+        {
+            if (context.SerialNumbers.Any())
+                return;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                context.SerialNumbers.Add(new SerialNumberModel
+                {
+                    SerialNumber = $"ACME-{i:000}"
+                });
+            }
+
+            context.SaveChanges();
+
+
+        }
+
+
+    }
+
+}
