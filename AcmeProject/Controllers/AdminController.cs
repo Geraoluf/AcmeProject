1eaf53e6ad1eaa0f432593906644df547732d7b2
gert Rasmussen <pjente@gmail.com>
2026-04-27 09:33:26 +0200

    getsubmissionview

---
+++ AcmeProject/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using ClassLibrary;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AcmeProject.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        private readonly AppDbContext _appDbContext;
+
+
+        public AdminController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetSubmission()
+        {
+            var submissions = _appDbContext.SubmissionModels.ToList();
+            return View(submissions);
+        }
+    }
+}
